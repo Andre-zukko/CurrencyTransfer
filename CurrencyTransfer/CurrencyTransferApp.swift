ed98b2a2677d23a616741f616eed86a57c6a4bfb
André Zubko <andrezubko03@gmail.com>
2024-02-24 00:08:15 +0100

    Initial Commit

---
+++ CurrencyTransfer/CurrencyTransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyTransferApp.swift
+//  CurrencyTransfer
+//
+//  Created by André Zubko on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyTransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
